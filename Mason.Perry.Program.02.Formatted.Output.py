424f612ae76afa1fe5c0392c41cbc5c82be308db
masonperry <mason_perry@my.uri.edu>
2016-02-29 23:07:44 -0500

    Update and rename Mason.Perry.Program.01.Formatted.Output.py to Mason.Perry.Program.02.Formatted.Output.py

---
+++ Mason.Perry.Program.02.Formatted.Output.py
@@ -0,0 +1,30 @@
+#Mason Perry
+#Program02
+
+print("Welcome!")                                   #welcome line
+
+input1 = int(input("Enter a number: "))             #user input one
+input2 = int(input("Enter a number: "))             #user input two
+
+#Adding
+add = input1 + input2
+if add:                                             #if statement
+    print(input1,"+",input2,"=",add)                #prints the line of addition
+
+#Subtracting
+subtract = input1 - input2
+if subtract:                                        #if statement
+    print(input1,"-",input2,"=", subtract)          #prints the line of subtraction
+
+#Multipling
+multiply = input1 * input2
+if multiply:                                        #if statement
+    print(input1,"*",input2,"=",multiply)           #prints the line of multiplication
+
+#Dividing
+divide = input1 / input2
+if divide:                                          #if statement
+    print(input1,"/",input2,"=",divide)             #prints the line of division
+
+#end
+
